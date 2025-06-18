d81e91daa0d75a62ffe3d32c49dbfdbd00d0cc0b
Georgi <georgi@Georgis-MacBook-Air.local>
2025-06-18 23:48:53 +0300

    updates

--- obj/Release/net9.0/dolapwebapp.AssemblyInfo.cs
+++ obj/Release/net9.0/dolapwebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dolapwebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d138db42843e7fc038e292f07fbd3c43a24c2fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("dolapwebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dolapwebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
